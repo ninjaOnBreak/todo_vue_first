1b1d20ee2c285344924728528e0a502d0f4aad72
ninjaOnBreak <ninjaonbreak.tools@gmail.com>
2021-10-17 01:43:25 +0200

    delete about/home and change title

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import App from './App.vue';
-import router from './router';
+// import router from './router';
 
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
@@ -11,6 +11,5 @@ library.add(faTrashAlt);
 Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 new Vue({
-  router,
   render: (h) => h(App),
 }).$mount('#app');

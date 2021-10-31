c7d888d2e895fdce653aaf91cef8bc18bb9377b0
ninjaOnBreak <ninjaonbreak.tools@gmail.com>
2021-10-31 16:35:33 +0100

    change styling

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import App from './App.vue';
+import 'boxicons';
 
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
